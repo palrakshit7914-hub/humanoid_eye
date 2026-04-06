a13b48ab4d14a692c7b61b5dd39981c56560d463
Rakshit <palrakshit7914@gmail.com>
2026-04-06 22:24:15 +0530

    refactor camera setup and configuration for ESP32 integration

--- camera.cpp
+++ camera.cpp
@@ -1,2 +1,48 @@
 #include "esp_camera.h"
 #include <WiFi.h>
+
+const char* ssid = "humanoid_eye";
+const char* password = "imi";
+
+// Camera model(AI Thinker)
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = 5;
+  config.pin_d1 = 18;
+  config.pin_d2 = 19;
+  config.pin_d3 = 21;
+  config.pin_d4 = 36;
+  config.pin_d5 = 39;
+  config.pin_d6 = 34;
+  config.pin_d7 = 35;
+  config.pin_xclk = 0;
+  config.pin_pclk = 22;
+  config.pin_vsync = 25;
+  config.pin_href = 23;
+  config.pin_sscb_sda = 26;
+  config.pin_sscb_scl = 27;
+  config.pin_pwdn = 32;
+  config.pin_reset = -1;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+    if(psramFound()){
+        config.frame_size = FRAMESIZE_VGA;
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+    } else {
+        config.frame_size = FRAMESIZE_CIF;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
