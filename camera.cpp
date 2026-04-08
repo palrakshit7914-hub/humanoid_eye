3c0e2f857887ef691bce6be1253289545d8e1d64
Rakshit <palrakshit7914@gmail.com>
2026-04-08 11:56:48 +0530

    add camera initialization and error handling in setup

--- camera.cpp
+++ camera.cpp
@@ -46,3 +46,12 @@ void setup() {
         config.jpeg_quality = 12;
         config.fb_count = 1;
     }
+
+    // Camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+
+    
